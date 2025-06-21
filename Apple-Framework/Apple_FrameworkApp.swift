e100e7cd70998a7d82d443cdafceada2d2f69522
HDALBYK <alby.kennady@harley-davidson.com>
2025-06-21 15:48:09 +0530

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Alby Kennady on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-FrameworkTests/Apple_FrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkTests.swift
+//  Apple-FrameworkTests
+//
+//  Created by Alby Kennady on 21/06/25.
+//
+
+import Testing
+@testable import Apple_Framework
+
+struct Apple_FrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
